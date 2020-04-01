2df88e646829429ca335e591278af25b813ccbea
Jason Reilly <jdr0887@gmail.com>
2020-04-01 04:54:35 -0400

    incrementing building of model

--- src/bin/logistic_regression_rustlearn.rs
+++ src/bin/logistic_regression_rustlearn.rs
@@ -9,11 +9,12 @@ extern crate serde_derive;
 extern crate structopt;
 
 use humantime::format_duration;
+use itertools::Itertools;
 use log::Level;
 use rustlearn::array;
 use rustlearn::linear_models::sgdclassifier;
-use rustlearn::metrics;
 use rustlearn::prelude::*;
+use std::collections;
 use std::fs;
 use std::io;
 use std::path;
@@ -44,20 +45,38 @@ fn main() -> io::Result<()> {
     let mut train_metadata_path = train_dir.clone();
     train_metadata_path.push("metadata.json");
 
+    debug!("reading: {}", train_metadata_path.to_string_lossy());
     let train_metadata_file = fs::File::open(train_metadata_path.as_path()).expect("missing file");
     let br = io::BufReader::new(train_metadata_file);
 
-    let metadata: rusty_herbarium::TrainMetadata = serde_json::from_reader(br)?;
+    let training_metadata: rusty_herbarium::TrainMetadata = serde_json::from_reader(br)?;
 
-    // let size = (520u32, 660u32);
-    //let size = (315u32, 400u32); // killed at 35k
-    //let size = (284u32, 360u32); // killed at 70k
-    //let size = (236u32, 300u32);
-    let size = (210u32, 264u32);
+    let image_ids_by_category_map: collections::HashMap<i32, Vec<i32>> =
+        training_metadata.annotations.iter().map(|x| (x.category_id, x.image_id)).into_group_map();
 
-    let (train_data, train_labels) = rusty_herbarium::normalized_train_data(&train_dir, &metadata, size.0, size.1)?;
+    let mut category_ids: Vec<_> = image_ids_by_category_map.keys().cloned().collect();
+    category_ids.sort();
 
-    let col_size = ((size.0 * size.1) * 3) as usize;
+    let mut image_path_by_category_map: collections::BTreeMap<i32, Vec<path::PathBuf>> = collections::BTreeMap::new();
+
+    for i in category_ids.iter() {
+        let image_ids = image_ids_by_category_map.get(i).unwrap();
+        // only grabbing 2 images per category...for now
+        let filtered_images_ids: Vec<_> = image_ids.iter().take(2).collect();
+        for image_id in filtered_images_ids.into_iter() {
+            let image = training_metadata.images.iter().find(|e| e.id == *image_id).unwrap();
+            let mut image_path = train_dir.clone();
+            image_path.push(image.file_name.clone());
+            image_path_by_category_map.entry(*i).or_insert(Vec::new()).push(image_path);
+        }
+    }
+
+    // let width = 315u32;
+    // let height = 390u32;
+    let width = 242u32;
+    let height = 300u32;
+
+    let col_size = ((width * height) * 3) as usize;
 
     let mut model = sgdclassifier::Hyperparameters::new(col_size)
         .learning_rate(0.1)
@@ -65,7 +84,55 @@ fn main() -> io::Result<()> {
         .l2_penalty(0.0)
         .one_vs_rest();
 
-    model.fit(&train_data, &train_labels).unwrap();
+    let image_path_by_category_map_keys: Vec<_> = image_path_by_category_map.keys().cloned().collect();
+
+    for chunk in image_path_by_category_map_keys.chunks(100) {
+        let mut labels: Vec<f32> = Vec::with_capacity(chunk.len());
+        let mut train_data = array::sparse::SparseRowArray::zeros(chunk.len(), col_size);
+
+        for (i, category_id) in chunk.iter().enumerate() {
+            labels.push(*category_id as f32);
+            for image_paths in image_path_by_category_map.get(category_id) {
+                for image_path in image_paths.iter() {
+                    debug!("category_id: {}, image_path: {}", category_id, image_path.to_string_lossy());
+
+                    let img = image::open(image_path.as_path()).unwrap();
+                    // risk cropping more from the bottom as roots don't offer identifying species features
+                    // stems, leafs, and flowers are where it's at
+                    let mut cropped_image = rusty_herbarium::crop_image(img, 30, 30, 80, 140);
+                    // original images are roughly 680x1000
+                    // resulting cropping will return roughly 620x780
+
+                    image::imageops::invert(&mut cropped_image);
+                    image::imageops::brighten(&mut cropped_image, 30);
+                    image::imageops::contrast(&mut cropped_image, 60.0);
+
+                    let resized_image = image::imageops::resize(&cropped_image, width, height, image::imageops::FilterType::Gaussian);
+                    let mut idx = 0;
+                    for x in 0..resized_image.width() {
+                        for y in 0..resized_image.height() {
+                            let pixel = resized_image.get_pixel(x, y);
+                            let red = pixel[0];
+                            let green = pixel[1];
+                            let blue = pixel[2];
+
+                            if red > 20 && green > 20 && blue > 20 {
+                                train_data.set(i, idx, red as f32 / 255.0);
+                                train_data.set(i, idx + 1, green as f32 / 255.0);
+                                train_data.set(i, idx + 2, blue as f32 / 255.0);
+                            }
+                            idx += 3;
+                            // debug!("i: {}, idx: {}", i, idx);
+                        }
+                    }
+                }
+            }
+        }
+
+        let start_fitting = Instant::now();
+        model.fit(&train_data, &array::dense::Array::from(labels)).unwrap();
+        debug!("model fitting duration: {}", format_duration(start_fitting.elapsed()).to_string());
+    }
 
     info!("Duration: {}", format_duration(start.elapsed()).to_string());
     Ok(())
